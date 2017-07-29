613b68ee57a39307a735fb46539763c52acef8c2
lucasap <lucas.sapoznik@hotmail.com>
2017-07-29 15:38:31 -0300

    API

---
+++ ApptivoAPI/API/API/Controllers/ApptivoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using API.Models;
+
+namespace API.Controllers
+{
+    public class ApptivoController : Controller
+    {
+        // GET: Apptivo
+        public ActionResult Registro()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Registro(Usuario Nusuario)
+        {
+            Nusuario.AgregarUsuario(Nusuario.Email);
+            ViewBag.Nombre = Nusuario.Nombre;
+            ViewBag.Email = Nusuario.Email;
+            //return RedirectToAction("RegistroCorrecto",Nusuario);
+            return View("RegistroCorrecto", Nusuario);
+        }
+        public ActionResult RegistroCorrecto()
+        {
+            return View();
+        }
+    }
+}
